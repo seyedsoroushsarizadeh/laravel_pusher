134f46452c9edac0c4a314d3058119efdc195fff
seyed soroush sarizadeh <seyed.soroush.sarizadeh@gmail.com>
2021-08-07 11:30:31 +0430

    complete-biuld-one

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\StatusLiked;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
+});
+
+Route::get('test', function () {
+    event(new StatusLiked('soroush'));
+    return "Event has been sent!";
 });
